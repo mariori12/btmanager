6a2964e929206d4b7bc02e16f7bbb1d1a3f3e50c
Hideki Miyamoto <miyamoto@fukoku-net.co.jp>
2022-10-26 15:58:26 +0900

    Add: ログイン状態を保持する機能を追加する
    ログイン状態を保持する機能を追加する。

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   include CsvOperatable
 
+  attr_accessor :remember_token
+
   has_secure_password
 
   has_many :body_temperatures, dependent: :destroy
@@ -13,6 +15,33 @@ class User < ApplicationRecord
   validates :password, presence: true, allow_nil: true
 
   scope :sorted, -> { order(id: :asc) }
+
+
+  def create_remember_digest
+    self.remember_token = User.new_token
+    self.remember_digest = User.digest(remember_token)
+    save!
+  end
+
+  def delete_remember_digest
+    self.remember_digest = nil
+    save!
+  end
+
+  def authenticated?(remember_token)
+    return false if remember_digest.nil?
+
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  def self.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
+
+  def self.new_token
+    SecureRandom.urlsafe_base64
+  end
 
   private
 
